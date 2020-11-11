996ec21381f1114c4ce6d23fa37c7039ce923779
Adelaide Nalley <adiainthesky@gmail.com>
2020-11-10 20:05:49 -0800

    clean up scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
-// function createGrid() {
-//   const div = document.createElement("div");
-//   div.className = "square"
-//   div.innerHTML = "value"
-// }
-
 function createGrid() {
   for (let rows = 0; rows < 256; rows++) {
     $("#grid").append("<div class='square'></div>");
   };
 };
 
+$(document).ready(function(){
+  createGrid();
+  $('.square').hover(function(){
+      $(this).addClass('hover');
+  })
+});
+
+// another option start:
+// function createGrid() {
+//   const div = document.createElement("div");
+//   div.className = "square"
+//   div.innerHTML = "value"
+// }
 
 // Marni's
 // function createGrid() {
@@ -20,23 +27,4 @@ function createGrid() {
 //     grid.appendChild(square); 
 //   }
 // }
-  // for (col=0; col < 16; col++) {
-  //   // for (row=0; row <16; row++) {
-  //     let square = document.createElement("div");
-  //     square.classList.add("square");
-  //     container.appendChild(square);
-  // }
-  //   // }
-/* 
-} */
 
-
-
-
-// HOVER --works
-$(document).ready(function(){
-  createGrid();
-  $('.square').hover(function(){
-      $(this).addClass('hover');
-  })
-});
